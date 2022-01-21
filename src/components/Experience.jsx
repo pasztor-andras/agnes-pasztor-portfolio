e8dac9fed43da4e163359fac9402d2288c2e141d
pasztor-andras <andras.pasztor1@gmail.com>
2022-01-21 16:46:40 +0100

    refactoring

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,35 +4,41 @@ export const Experience = () => {
   return (
     <div className="exp-container">
       <span>Experience timeline</span>
-      {/* <div className="exp-cards-container">
-        <div className="exp-card"></div>
-        <div className="exp-card"></div>
-        <div className="exp-card"></div>
-      </div>
-      <span className="horizontal-line"></span> */}
       <div className="exp-timeline">
         <div className="exp-card left">
           <div className="content">
             <h2>2017</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam
+              impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.
+            </p>
           </div>
         </div>
         <div className="exp-card right">
           <div className="content">
             <h2>2016</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam
+              impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.
+            </p>
           </div>
         </div>
         <div className="exp-card left">
           <div className="content">
             <h2>2017</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam
+              impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.
+            </p>
           </div>
         </div>
         <div className="exp-card right">
           <div className="content">
             <h2>2016</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur fugiat unde, odit nesciunt expedita amet reprehenderit facere ipsam
+              impedit ea voluptas, incidunt aspernatur, aliquid voluptatum sunt! Harum nemo deserunt quidem.
+            </p>
           </div>
         </div>
       </div>
