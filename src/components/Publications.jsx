e2ca3b626e795b6d71014f8fe729c7d751f5376a
pasztor-andras <andras.pasztor1@gmail.com>
2022-01-27 15:58:38 +0100

    add skills styling, links, debugging

--- src/components/Publications.jsx
+++ src/components/Publications.jsx
@@ -10,8 +10,12 @@ export const Publications = () => {
           <span className="circle color-green"></span>
           <p>Publications & Articles</p>
         </div>
-        <a href="https://scholar.google.com/citations?user=4vZlm9kAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">Publications</a>
-        <a href="https://medium.com/amboss/quality-engineering-70213186d4f4" target="_blank" rel="noopener noreferrer">Recoding Engineering Culture</a>
+        <a href="https://scholar.google.com/citations?user=4vZlm9kAAAAJ&hl=en" target="_blank" rel="noopener noreferrer">
+          Publications
+        </a>
+        <a href="https://medium.com/amboss/quality-engineering-70213186d4f4" target="_blank" rel="noopener noreferrer">
+          Recoding Engineering Culture
+        </a>
       </div>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,23 +1,47 @@
 import React from "react";
+import dataeng2 from "../icons/dataeng2.png";
+import dataeng3 from "../icons/dataeng3.png";
+import python from "../icons/002-python.png";
 import sql from "../icons/001-sql.png";
+import js from "../icons/001-javascript.png";
+import cypress from "../icons/cypress-black.png";
 import selenium from "../icons/003-selenium.png";
-import js from "../icons/005-javascript.png";
-import api from "../icons/010-api-1.png";
+import java from "../icons/005-javascript.png";
+import bash from "../icons/bash.png";
 
 export const Skills = () => {
   return (
     <div className="mid-section">
       <div className="skills-container">
-        <p>Data Engineering</p>
-        <p>Python</p>
-        <p>SQL</p>
+        <div className="skills-background">
+          <img className="icons" src={dataeng2} alt="data engineering" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={dataeng3} alt="data engineering" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={python} alt="python" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={sql} alt="sql" />
+        </div>
       </div>
       <div className="skills-container">
-        <p>JS</p>
-        <p>Cypress</p>
-        <p>Selenium</p>
-        <p>Java</p>
-        <p>Bash</p>
+        <div className="skills-background">
+          <img className="icons" src={js} alt="" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={cypress} alt="" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={selenium} alt="" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={java} alt="" />
+        </div>
+        <div className="skills-background">
+          <img className="icons" src={bash} alt="" />
+        </div>
       </div>
     </div>
   );
