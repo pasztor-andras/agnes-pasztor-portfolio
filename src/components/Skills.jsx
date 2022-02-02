41462d3be2c6205cc17270ae2bc2b6a548aaf25b
pasztor-andras <andras.pasztor1@gmail.com>
2022-02-02 11:48:09 +0100

    .

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,55 +1,29 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-
 export const Skills = () => {
-
   const iconNames = ["dataeng", "python", "sql", "js", "cypress", "selenium", "java", "bash", "linux"];
-
-
-  const rowTwo = iconNames.slice(4, 9).map((icon, index) => {
-    return (
-      <div
-        className="skills-background"
-        key={index}
-        initial={{ scale: 1 }}
-        whileHover={{ scale: 1.2} }   
-      >
-        <img className="icons" key={index} alt={icon} src={require(`../icons/${icon}.png`)}></img>
-      </div>
-    );
-  });
 
   return (
     <div className="mid-section">
-      <div className="skills-container">{
-      iconNames.slice(0, 4).map((icon, index) => {
-        return (
-          <motion.div
-            className="skills-background"
-            key={index}
-            initial={{ scale: 1 }}
-            whileHover={{ scale: 1.2} }            
-          >
-            <img className="icons" key={index} alt={icon} src={require(`../icons/${icon}.png`)} animate={{}}></img>
-          </motion.div>
-        ); 
-      })
-      }</div>
-      <div className="skills-container">{
-        iconNames.slice(4, 9).map((icon, index) => {
+      <div className="skills-container">
+        {iconNames.slice(0, 4).map((icon, index) => {
           return (
-            <motion.div
-              className="skills-background"
-              key={index}
-              initial={{ scale: 1 }}
-              whileHover={{ scale: 1.2} }            
-            >
+            <motion.div className="skills-background" key={index} initial={{ scale: 1 }} whileHover={{ scale: 1.2 }}>
               <img className="icons" key={index} alt={icon} src={require(`../icons/${icon}.png`)} animate={{}}></img>
             </motion.div>
-          ); 
-        })
-      }</div>
+          );
+        })}
+      </div>
+      <div className="skills-container">
+        {iconNames.slice(4, 9).map((icon, index) => {
+          return (
+            <motion.div className="skills-background" key={index} initial={{ scale: 1 }} whileHover={{ scale: 1.2 }}>
+              <img className="icons" key={index} alt={icon} src={require(`../icons/${icon}.png`)} animate={{}}></img>
+            </motion.div>
+          );
+        })}
+      </div>
     </div>
   );
 };
