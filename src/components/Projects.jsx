e1e284395c40129b1de8e1ffab2e0eed297f7787
pasztor-andras <andras.pasztor1@gmail.com>
2022-02-14 17:03:36 +0100

    add projects logo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import amboss from "../images/Amboss.png";
+import omixon from "../images/omixon-logo.png"
 
 export const Projects = () => {
   const projectOneUrl = "https://www.amboss.com/us";
@@ -8,18 +10,20 @@ export const Projects = () => {
     <div className="mid-section project-container">
       <div className="project-card">
         <div className="project-card-inner">
-          <h1>Amboss</h1>
+          <img className="projects-logo" src={amboss} alt="amboss" />
+          {/* <h1>Amboss</h1>
           <a href={projectOneUrl} target="_blank" rel="noopener noreferrer">
             {projectOneUrl}
-          </a>
+          </a> */}
         </div>
       </div>
       <div className="project-card">
         <div className="project-card-inner">
-          <h1>Omixon</h1>
+          <img src={omixon} alt="omixon" />
+          {/* <h1>Omixon</h1>
           <a href={projectTwoUrl} target="_blank" rel="noopener noreferrer">
             {projectTwoUrl}
-          </a>
+          </a> */}
         </div>
       </div>
     </div>
